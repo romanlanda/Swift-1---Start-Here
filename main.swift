d767f24dc826c0779fb387c4b19972e6ed5b087c
Roman Landa <landagalan@gmail.com>
2022-08-05 19:42:17 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+var hello = "Hello, Roma!"
+
+var firstName = "Tom"
+print(hello)
+
+var stockPrice = 100
+print(stockPrice)
+
+let lastName = "Cruz"
+print("Hello \(firstName) \(lastName)! It's really nice to meet you!")
